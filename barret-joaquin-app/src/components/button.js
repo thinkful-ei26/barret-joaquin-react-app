1830003d8717816f00aa4e5b0a8004bb2e06a2c6
Joaquin Fox <joaquin.fox@gmail.com>
2018-12-12 09:23:59 -0700

    first button

---
+++ barret-joaquin-app/src/components/button.js
@@ -0,0 +1,10 @@
+'use strict';
+import React from 'react';
+
+export default function Button() {
+  return(
+  <div>
+    <button>Our first button</button>
+  </div>
+  )
+}
--- barret-joaquin-app/src/components/navigation.js
+++ barret-joaquin-app/src/components/navigation.js
@@ -1,22 +1,16 @@
 import React from 'react';
+import Button from './button';
 
 
 export default function Navigation(){
 
 
 
+    return (
+        <div>
+         <Button/>
+        </div>
 
-
-
-render(){
-return (
-<div>
-'Some output'
-</div>
-
-)
-
-
-}
+    )
 
 }
