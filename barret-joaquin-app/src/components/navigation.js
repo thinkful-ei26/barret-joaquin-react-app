d15b3f9a7fd7d5555e50d63a6390d3df39840bfc
Joaquin Fox <joaquin.fox@gmail.com>
2018-12-12 10:18:26 -0700

    button state working

--- barret-joaquin-app/src/components/navigation.js
+++ barret-joaquin-app/src/components/navigation.js
@@ -3,26 +3,29 @@ import Button from './button';
 import ButtonOutput from './button-output';
 
 export default class Navigation extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            buttonClicked: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonClicked: false
+    };
+  }
 
-    handleButtonClicked(){
-        console.log('parent heard the click');
-    }
+  handleButtonClicked() {
+    this.setState({ buttonClicked: !this.state.buttonClicked });
+    console.log(this.state.buttonClicked);
+    // console.log('parent heard the click');
+  }
 
-    render(){
-        return (
-            <div>
-             <Button buttonHandler={e =>this.handleButtonClicked()}/>
-             <ButtonOutput />
-            </div>
-    
-        )
+  render() {
+    if (this.state.buttonClicked) {
+      return (
+        <div>
+          <Button buttonHandler={e => this.handleButtonClicked()} />
+          <ButtonOutput />
+        </div>
+      );
+    } else {
+      return <Button buttonHandler={e => this.handleButtonClicked()} />;
     }
-    
-
+  }
 }
