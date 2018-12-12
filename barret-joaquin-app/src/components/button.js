1f0cfad9133f6f49221ae0e47b147569c8463dd8
Joaquin Fox <joaquin.fox@gmail.com>
2018-12-12 10:43:07 -0700

    two button functionality begin

--- barret-joaquin-app/src/components/button.js
+++ barret-joaquin-app/src/components/button.js
@@ -1,10 +1,14 @@
-'use strict';
 import React from 'react';
 
 export default function Button(props) {
   return(
-  <div>
-    <button onClick={e => props.buttonHandler()}>Our first button</button>
-  </div>
+  <section>  
+    <div>
+       <button id="first-button" onClick={e => props.firstButtonHandler()}>Our first button</button>
+    </div>
+    <div>
+     <button id="second-button"onClick={e => props.secondButtonHandler()}>Our second button</button>
+    </div>
+  </section>
   )
 }
--- barret-joaquin-app/src/components/navigation.js
+++ barret-joaquin-app/src/components/navigation.js
@@ -6,26 +6,37 @@ export default class Navigation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      buttonClicked: false
+      firstButtonClicked: false,
+      secondButtonClicked:false
     };
   }
 
-  handleButtonClicked() {
-    this.setState({ buttonClicked: !this.state.buttonClicked });
-    console.log(this.state.buttonClicked);
+  handleFirstButtonClicked() {
+    this.setState({ firstButtonClicked: !this.state.firstButtonClicked });
+    console.log(this.state.firstButtonClicked);
+    // console.log('parent heard the click');
+  }
+
+   handleSecondButtonClicked() {
+    this.setState({ secondButtonClicked: !this.state.secondButtonClicked });
+    // console.log(this.state.buttonClicked);
     // console.log('parent heard the click');
   }
 
   render() {
-    if (this.state.buttonClicked) {
+    if (this.state.firstButtonClicked||this.state.secondButtonClicked) {
       return (
         <div>
-          <Button buttonHandler={e => this.handleButtonClicked()} />
+          <Button id='first-button' firstButtonHandler={e => this.handleFirstButtonClicked()} />
+            <Button id='second-button' secondButtonHandler={e => this.handleButtonClicked()} />
           <ButtonOutput />
         </div>
       );
     } else {
-      return <Button buttonHandler={e => this.handleButtonClicked()} />;
+        
+      return <Button 
+      secondButtonHandler={e => this.handleSecondButtonClicked()}
+      firstButtonHandler={e => this.handleFirstButtonClicked()} />;
     }
   }
 }
